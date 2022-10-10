261ff1d7579207bbe9f2b4a29664fd2233efe072
Rishav <rishavsinha380@gmail.com>
2022-10-10 12:11:59 +0530

    Java Source File

---
+++ Geekyshows.java
@@ -0,0 +1,10 @@
+class Geekyshows
+    {
+              
+         public static void main (String args[])
+
+          {
+                System.out.println("Hello Geekyshows");
+          
+          }
+    }
