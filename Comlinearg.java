ff2b57bd5a9224d218124865b39dd28d6dae76ea
Rishav <rishavsinha380@gmail.com>
2022-10-11 15:04:30 +0530

    Java Source File

---
+++ Comlinearg.java
@@ -0,0 +1,8 @@
+class Comlinearg
+{
+          public static void main(String args[])
+             {
+                  System.out.println(" The argument is " + args[0] + " " +args[1]);
+
+             }
+}
