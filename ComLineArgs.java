fd920ead1364c317494a1e4ee6a29861610e1e88
Rishav <rishavsinha380@gmail.com>
2022-10-14 14:24:35 +0530

    Java Source File

---
+++ ComLineArgs.java
@@ -0,0 +1,9 @@
+class ComLineArgs
+{
+       public static void main(String args[])
+          { 
+              int n = Integer.parseInt(args[0]);         
+
+              System.out.println(" The value is " +n);
+          }
+}
