112c95e146e6f172d654d81bdce6f653fe055a9f
Rishav <rishavsinha380@gmail.com>
2022-10-16 11:43:54 +0530

    Java Source File

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+class EvenOdd
+{
+       static public void main(String args[])
+         {
+            int num = Integer.parseInt(args[0]);
+             
+            if(num%2==0)
+            System.out.println("No. is Even");
+            else
+            System.out.println("No. is Odd");
+         }
+}
