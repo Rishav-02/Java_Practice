5e36b6ed574208bfb81f138aa5ec6824b2e02083
Rishav <rishavsinha380@gmail.com>
2022-10-22 20:37:35 +0530

    Java Source File

---
+++ NumberSystemMain.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class NumberSystem
+{
+  int i,n,r,rev=0;
+  Scanner obj=new Scanner(System.in);
+
+  void input()
+      {
+        System.out.println("Enter a Number");
+        n=obj.nextInt();
+      }
+
+  void palindrome()
+      {
+        for(i=n;i>0;)
+         {
+           r=i%10;
+           rev=rev*10+r;
+           i=i/10;
+         }
+        
+        if(n==rev)
+           System.out.println("Number is palindrome");
+        else
+           System.out.println("Number is not palindrome");
+      }
+}
+
+class NumberSystemMain
+{
+    public static void main(String args[])
+         {
+            NumberSystem nobj=new NumberSystem();
+          
+            nobj.input();
+            nobj.palindrome();
+         }
+}
+
+          
+
