3e151537fefb81bcf9fb3dea373b5fb9cfb3e1e7
Rishav <rishavsinha380@gmail.com>
2022-10-24 14:04:57 +0530

    Java Source File

---
+++ SquareRoot.java
@@ -0,0 +1,15 @@
+//This code computes square root
+
+import java.lang.Math;
+ 
+class SquareRoot
+{
+      public static void main(String args[])
+      {
+           double x=5;      //Declaration and Initialization
+           double y;       //simple declaration
+
+           y=Math.sqrt(x);
+           System.out.println("y="+y);
+      }
+}
