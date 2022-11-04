7f6634a32084cb7720ad4ab1c05fc53a94c1414b
Rishav <rishavsinha380@gmail.com>
2022-11-04 22:50:00 +0530

    Java Source File

---
+++ sp10.java
@@ -0,0 +1,17 @@
+class sp10
+{
+  public static void main(String args[])
+   {
+     int x=0;
+     int y=0;
+     ++x;
+     System.out.println("x=0;++x;\t\t\t ->" +x);
+       x=0;     x++;
+     System.out.println("x=0;x++;\t\t\t ->" +x);
+       x=0;     --x;
+     System.out.println("x=0;--x;\t\t\t ->" +x);
+       x=0;     x--;
+     System.out.println("x=0;x--;\t\t\t ->" +x);
+   }
+}
+  
