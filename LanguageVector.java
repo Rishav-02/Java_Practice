b2cef4f7ca557ebd5fc04798127b80866c6830e7
Rishav <rishavsinha380@gmail.com>
2022-11-09 11:02:55 +0530

    Java Source File

---
+++ LanguageVector.java
@@ -0,0 +1,22 @@
+import java.util.*;                           //Importing Vector class
+class LanguageVector
+{
+   public static void main(String args[])
+   {
+      Vector list=new Vector( );
+      int length=args.length;                 //Command line argument
+      for(int i=0;i<length;i++)
+      {
+        list.addElement(args[i]);
+      }
+      list.insertElementAt("COBOL" ,2);
+      int size=list.size( );
+      String listArray[]=new String[size];
+      list.copyInto(listArray);
+      System.out.println("List of Languages");
+      for(int i=0;i<size;i++)
+      {
+        System.out.println(listArray[i]);
+      }
+   }
+}
