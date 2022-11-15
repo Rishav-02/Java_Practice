5e4ee81b967526aba99773e8dbc485d1de582810
Rishav <rishavsinha380@gmail.com>
2022-11-15 12:06:12 +0530

    Java Source File

---
+++ package Program/ImportClass.java
@@ -0,0 +1,9 @@
+import circle;
+class ImportClass
+{
+   public static void main(String[] args)
+   {
+      circle.NewCircle nc=new circle.NewCircle();
+      System.out.println("Hello World!");
+   }
+}
