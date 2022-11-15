4f59985b53b06e3037246f0da94474bb0fae61cb
Rishav <rishavsinha380@gmail.com>
2022-11-15 12:07:39 +0530

    Java File

---
+++ package Program/Circle/NewCircle.java
@@ -0,0 +1,11 @@
+package circle;
+class NewCircle
+{
+    public void draw(){}
+    public void getRadius(){}
+
+    public static void main(String args[])
+     {
+      System.out.println("Package Creation done");
+     }
+}
